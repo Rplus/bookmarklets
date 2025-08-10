ae2b851aa256dcc1f53df9f953f0f590df776f6f
Rplus <rplus.tw@gmail.com>
2025-08-10 23:47:56 +0800

    fix sha256

--- greasemonkey/bd2-news-viewer.js
+++ greasemonkey/bd2-news-viewer.js
@@ -3,12 +3,12 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.browndust2.com/robots.txt
 // @grant       none
-// @version     1.6.1
+// @version     1.6.2
 // @author      Rplus
 // @description custom news viewer for sucking browndust2.com
 // @require     https://unpkg.com/localforage@1.10.0/dist/localforage.min.js#sha384-MTDrIlFOzEqpmOxY6UIA/1Zkh0a64UlmJ6R0UrZXqXCPx99siPGi8EmtQjIeCcTH
 // @require     https://unpkg.com/marked@16.1.2/lib/marked.umd.js#sha256-XF49G0iwZLps3m+Jg99uZv4DSzMQYF1NxTXcT0XXjmU=
-// @require     https://unpkg.com/dompurify@1.0.8/dist/purify.min.js#sha256-ieH6dkfLSVNw06mXrOQ4f10V2fTFrxI1LFPapACVYoc=
+// @require     https://unpkg.com/dompurify@1.0.8/dist/purify.min.js#sha256-WGpUzZ0j5KVvf7ufGyKpoVoWxa4B5dpoMFYoZh2+ndk=
 // @@run-at     document-end
 // @license     WTFPL
 // ==/UserScript==
